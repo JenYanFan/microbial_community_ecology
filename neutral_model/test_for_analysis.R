c0011f68bac09e76398a04c2506548393ce9c280
Jen <fanjenyan@gmail.com>
2021-03-16 17:38:12 +0800

    fixed typo

--- neutral_model/test_for_analysis.R
+++ neutral_model/test_for_analysis.R
@@ -9,11 +9,19 @@ rarified_otu_count <- rarified_otu_count[rowSums(rarified_otu_count) > 0, ]
 
 # neutral distance ----------
 set.seed(19941119)
-neutral_distance <- calculate_neutral_distance(rarified_otu_count, replication = 20)
-neutral_distance <- lapply(neutral_distance, as.matrix)
-observed_distance <- as.matrix(vegdist(t(rarified_otu_count), method = "bray"))
+neutral_distance <- calculate_neutral_distance(rarified_otu_count, replication = 200)
+observed_distance <- vegdist(t(rarified_otu_count), method = "bray")
 raup_crick <- calculate_raup_crick(observed_distance, neutral_distance)
 deterministic_ratio <- calculate_deterministic_ratio(observed_distance, neutral_distance)
+
+neutral_boy <- data.frame(bray_cutis_dissimilarity = sapply(neutral_distance, function(x){return(x[1])}))
+
+
+ggplot(neutral_boy, aes(x = bray_cutis_dissimilarity)) +
+  labs(x = "Bray-Curtis dissimilarity", y = "Density") +
+  geom_vline(aes(xintercept = 0.84), linetype = "dashed") +
+  geom_density() +
+  theme_classic()
 
 raup_crick_dist <- distance_to_dataframe(raup_crick)
 DR_dist <- distance_to_dataframe(deterministic_ratio)
